313e7ef33ab5e54fb01d5c42431f3d3b763c3df3
GiangTruong <kemdeptrai1997@gmail.com>
2019-06-13 20:58:32 +0700

    Init routes for admin

--- routes/admin.php
+++ routes/admin.php
@@ -11,6 +11,86 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('',[
+    'as' => 'admin.dashboard.index',
+    'uses' => 'DashboardController@index'
+]);
+Route::group(['prefix' => 'products'], function () {
+    Route::get('',[
+        'as'=>'admin.products.index',
+        'uses'=>'ProductController@index'
+    ]);
+    Route::get('create',[
+        'as' => 'admin.products.create',
+        'uses'=>'ProductController@create'
+    ]);
+    Route::post('',[
+        'as'=>'admin.products.store',
+        'uses'=>'ProductController@store'
+    ]);
+    Route::get('{id}/edit',[
+        'as' => 'admin.products.edit',
+        'uses'=>'ProductController@edit'
+    ]);
+    Route::put('{id}',[
+        'as'=>'admin.products.update',
+        'uses'=>'ProductController@update'
+    ]);
+    Route::delete('{id}',[
+        'as'=>'admin.products.destroy',
+        'uses'=>'ProductController@destroy'
+    ]);
+});
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('',[
+        'as'=>'admin.categories.index',
+        'uses'=>'CategoryController@index'
+    ]);
+    Route::get('create',[
+        'as' => 'admin.categories.create',
+        'uses'=>'CategoryController@create'
+    ]);
+    Route::post('',[
+        'as'=>'admin.categories.store',
+        'uses'=>'CategoryController@store'
+    ]);
+    Route::get('{id}/edit',[
+        'as' => 'admin.categories.edit',
+        'uses'=>'CategoryController@edit'
+    ]);
+    Route::put('{id}',[
+        'as'=>'admin.categories.update',
+        'uses'=>'CategoryController@update'
+    ]);
+    Route::delete('{id}',[
+        'as'=>'admin.categories.destroy',
+        'uses'=>'CategoryController@destroy'
+    ]);
+    
+    Route::group(['prefix' => 'orders'], function () {
+        Route::get('',[
+            'as'=>'admin.orders.index',
+            'uses'=>'OrderController@index'
+        ]); 
+        Route::get('processed',[
+            'as'=>'admin.orders.processed',
+            'uses'=>'OrderController@processed'
+        ]);
+        Route::get('{id}/edit',[
+            'as' => 'admin.orders.edit',
+            'uses'=>'OrderController@edit'
+        ]);
+        Route::put('{id}',[
+            'as'=>'admin.orders.update',
+            'uses'=>'OrderController@update'
+        ]);
+    });
+    Route::resource('users', 'UserController',[
+        'parameters' => [
+            'users' => 'id'
+        ],
+        'except'=>'show',
+        'as'=>'admin'
+    ]);
 });
